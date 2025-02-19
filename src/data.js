df1745b7b3cad69d09f7821715e980f51d521aa5
Anonymus-dev21 <oscab05@outlook.com>
2025-02-19 16:19:43 -0300

    ComienzoWebsockets en otra rama

--- src/data.js
+++ src/data.js
@@ -1,46 +1,4 @@
 
-// export const productos = [
-//     {
-//       id:1,
-//       title: "Producto 1",
-//       descripcion: "Descripción del producto 1",
-//       precio: 100,
-//       codigo: "15",
-//       stock: 10,
-      
-//     },
-//     {
-//       id:2,
-//       title: "Producto 2",
-//       descripcion: "Descripción del producto 2",
-//       precio: 200,
-//       codigo: "52",
-//       stock: 20,
-      
-//     },
-//     {
-//       id  :3,
-//       title: "Producto 3",
-//      descripcion: "Descripción del producto 3",
-//       precio: 300,
-//       codigo:"23",
-//       stock: 30,
-     
-//     }
-//   ];
-
-//     export const carrito = [
-//         {
-//             id,
-//             products: [
-//             {
-//               id,
-//               quantity
-//             }
-//           ]
-//           }
-//         ]
-
 import fs from "fs";
 
 const DATA_PATH = "./data.txt"; 
